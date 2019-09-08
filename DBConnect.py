72b8983e6a6070d17b4eda8166f1ea15b5597be3
Bradley Bruce <bradleyabruce@outlook.com>
2019-09-07 23:33:21 -0400

    Small Update
    - Added try/catch to database connection

--- DBConnect.py
+++ DBConnect.py
@@ -6,28 +6,31 @@ import urllib3
 
 # ---------------------- Database Call ----------------------------------
 
-# Connect to Sql Server
-conn = pyodbc.connect('Driver={SQL Server Native Client 11.0};'
+try:
+    conn = pyodbc.connect('Driver={SQL Server Native Client 11.0};'
                       'Server=174.101.154.93;'
                       'Database=PasswordManager;'
                       'uid=bruceba;'
                       'pwd=password')
 
-# Store return value of a database query in a Panda Data Frame
-value = pd.read_sql_query('select * from tEntries', conn)
-print(value.count())
+    # Store return value of a database query in a Panda Data Frame
+    value = pd.read_sql_query('select * from tEntries', conn)
+    print(value.count())
 
-# Foreach row value, create a Password Class and store it in a list of Password Classes
-DatabasePasswords = [
-    (Password.Password(row.EntryID, row.UserID, row.WebsiteDomainID, row.WebsitePasswordID, row.CategoryID))
-    for index, row in value.iterrows()]
+    # Foreach row value, create a Password Class and store it in a list of Password Classes
+    DatabasePasswords = [
+        (Password.Password(row.EntryID, row.UserID, row.WebsiteDomainID, row.WebsitePasswordID, row.CategoryID))
+        for index, row in value.iterrows()]
 
-# Foreach Password Class in list of Passwords, print out the EntryID an int
-for x in DatabasePasswords:
-    print(int(x.EntryID))
+    # Foreach Password Class in list of Passwords, print out the EntryID an int
+    for x in DatabasePasswords:
+        print(int(x.EntryID))
 
-# print out the length of the array not the count in this instance
-print('Count of Database Passwords: ' + str(len(DatabasePasswords)))
+    # print out the length of the array not the count in this instance
+    print('Count of Database Passwords: ' + str(len(DatabasePasswords)))
+
+except Exception:
+    print('Database Error!!!')
 
 # ------------------------ API Post Call ---------------------------------------
 
